772d4956cca45d914cc9301dd0c28cb8990c9090
CristRocha <crisrfab@gmail.com>
2018-12-31 17:30:41 -0200

    alt Jquery para Vanilla JS

--- js/script.js
+++ js/script.js
@@ -1,43 +1,59 @@
-var $thumbs = $('.thumbnails');
-var $lbImg = $('.lightbox-img');
-var $lb = $('.lightbox');
-var $btnClose = $('.btn-close');
-var $prod = $('#nameProd');
-var $efect = $('#efectProd')
-var $ingred = $('#ingr');
-var $preco = $('#preco');
-var $btnMenu = $('#menuSide');
-var $nav = $('#navMenu');
+window.onload = init;
 
-$thumbs.on('click', 'a',function(e){
-	e.preventDefault();
-	var big = $(this).attr('href');
-	$lbImg.attr('src',big);
-	$lb.attr('data-state','visible');
-    var indice = $(this).attr('data-title');
-	$.ajax({
-        url:'js/potions.json',
-        dataType:'json',
-        type:'get',
-        cache:false,
-        success:function(data){
-            for(var prop in data.potions){
-                if(prop == indice){
-            $prod.html(data.potions[prop].name);
-            $efect.html(data.potions[prop].effect);
-            $ingred.html(data.potions[prop].ingredients);
-            $preco.html(data.potions[prop].price);
+function init() {
+
+    const thumbs = document.querySelectorAll('.thumbnails');
+    const lbImg = document.querySelector('.lightbox-img');
+    const lb = document.getElementsByClassName('lightbox')[0];
+    const btnClose = document.getElementsByClassName('btn-close')[0];
+    const prod = document.getElementById('nameProd');
+    const efect = document.getElementById('efectProd');
+    const ingred = document.getElementById('ingr');
+    const preco = document.getElementById('preco');
+    const btnMenu = document.getElementById('menuSide');
+    const nav = document.getElementById('navMenu');
+
+    for (let el of thumbs) {
+        el.addEventListener('click', function (e) {
+            e.preventDefault();
+            modal = el.firstChild;
+            var indice = modal.getAttribute('data-title');
+            let imagem = modal.getAttribute('href');
+            lbImg.setAttribute('src', imagem);
+            lb.setAttribute('data-state', 'visible');
+
+            loadPotion(indice);
+        });
+    }
+    document.getElementById('btnAdd').addEventListener('click',function(){
+        addBag(1);
+    });
+
+    function loadPotion(indice) {
+        const json = new XMLHttpRequest();
+        json.open('GET', 'js/potions.json')
+
+        json.onload = function () {
+            let data = JSON.parse(this.response);
+            for (let prop in data.potions) {
+                if (prop == indice) {
+                    prod.innerHTML = data.potions[prop].name;
+                    efect.innerHTML = data.potions[prop].effect;
+                    ingred.innerHTML = data.potions[prop].ingredients;
+                    preco.innerHTML = data.potions[prop].price;
                 }
             }
         }
-    })
-});
+        json.send();
+    }
 
-$btnClose.on('click',function(){
-	$lb.attr('data-state','hidden');
-});
+    function addBag(quant){
+        let total = parseInt(document.getElementById('number').textContent) ;
+        total +=quant;
+        document.getElementById('number').textContent = total;
+    }
 
-$btnMenu.on('click',function(){
-    $nav.toggle();
-})
+    btnClose.onclick = () => lb.setAttribute('data-state', 'hidden');
 
+    btnMenu.onclick = () => nav.classList.toggle('hidden');
+}
